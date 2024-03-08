9b171c73153c895b98b060c4ef901834e013bb82
Arash7447 <arash.sotoudeh6@gmail.com>
2024-03-08 23:39:00 +0330

    added static files
    added static files

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 def index_view(request) :
     return render(request, "website/index.html")
